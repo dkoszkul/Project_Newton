799f1d46700a499da02f06d67e1145260fe60c9d
przedwojow <dominik.koszkul@yahoo.com>
2015-04-27 18:09:33 +0200

    wyrzucenie funkcji które źle testują nasz program

--- Rastrigin.m
+++
@@ -1,8 +0,0 @@
- function [y] = Rastrigin()
-%Goldstein-price Summary of this function goes here
-%   Detailed explanation goes here
-
-syms x x1 x2 x3;
-y =x1.^2 + x2.^2 - 10.*cos(2.*pi.*x1) - 10.*cos(2.*pi.*x2) + 20;
-
- end
--- drawButtonSwitchCase.m
+++ drawButtonSwitchCase.m
@@ -16,12 +16,6 @@ switch handles.current_data
     case 'Himmelblau modified function'
         [handles.results]=Himmelblau();
         set(handles.functionEquation,'String','(x1^2 + x2 - 11)^2 + (x1 + x2^2 - 7)^2');
-    case 'Ackley function'
-        [handles.results]=Ackley();
-        set(handles.functionEquation,'String','-a*e(-b*sqrt((x1^2 + x2^2)/2)-e((cos(c*x1) + cos(c*x2))/2)+a+e');
-    case 'Rastrigin function'
-        [handles.results]=Rastrigin();
-        set(handles.functionEquation,'String','x1^2 + x2^2 - 10*cos(2*pi*x1) - 10*cos(2*pi*x2) + 20');
     case 'Geem test function'
         [handles.results]=Geem();
         set(handles.functionEquation,'String','4*x1^2-2.1*x1^4+(1/3)*x1^6+x1*x2-4*x2^2+4*x2^4');
