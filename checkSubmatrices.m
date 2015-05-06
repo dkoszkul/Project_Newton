898b0044f2400c9fd3912313b7d3a86b6b92f80f
przedwojow <dominik.koszkul@yahoo.com>
2015-05-06 20:48:58 +0200

    sprawdzanie podmacierzy
    Sprawdzanie podmacierzy :
    kryterium Sylvestera.
    
    Jeżeli hesjan (macierz drugiej pochodnej) jest określony dodatnio (czyli
    wszystkie minory główne są dodatnie), to jest minimum, jeżeli zaś
    ujemnie (pierwszy minor ujemny, drugi dodatni i naprzemiennie), jest
    maksimum.

---
+++ checkSubmatrices.m
@@ -0,0 +1,33 @@
+function [ result ,wyznaczniki_podmacierzy] = checkSubmatrices( M )
+[m,n]=size(M);
+wyznaczniki_podmacierzy=zeros(m,1);
+result='local max';
+size_M=m;
+M_temp=M;
+%%%%%%%%%%%%%COUNTING DETERMINANTS OF SUBMATRICES%%%%%%%%%%%%%%%%
+for i=1:1:size_M
+    wyznaczniki_podmacierzy(i)=det(M_temp);
+    M_temp(:,n)=[];
+    M_temp(m,:)=[];
+    [m,n]=size(M_temp);
+end
+[x,y]=size(wyznaczniki_podmacierzy);
+%%%%%%%%%%%%%CHECK WHETHER IS IT A LOCAL MINIMUM%%%%%%%%%%%%%%%%
+if(min(wyznaczniki_podmacierzy)>0)
+    result='local min';
+else
+    %%%%%%%%%%%%%CHECK WHETHER IS IT A LOCAL MAXIMUM%%%%%%%%%%%%%%%%
+    for i=x:-1:1
+        numberOfSubmatrix = (x - i)+1;
+        if(mod(numberOfSubmatrix,2) == 0 && wyznaczniki_podmacierzy(i) < 0) %if i is even
+            result='other';
+            break;
+        else if(mod(numberOfSubmatrix,2) == 1 && wyznaczniki_podmacierzy(i) > 0)
+                result='other';
+                break;
+            end
+        end
+    end
+end
+end
+
