587dc8fb15f3d5e313d7daf285d0e8b8bfbc926e
przedwojow <dominik.koszkul@yahoo.com>
2015-05-09 12:06:43 +0200

    Sprawdzanie czy punkt jest min, max albo innym.
    Dodane pole pokazujące jaki to punkt. Jeżeli nie znaleziono punktu dane
    się nie pokazują bo są baardzo bez sensu z wiadomego powodu.

--- checkSubmatrices.m
+++ checkSubmatrices.m
@@ -1,7 +1,7 @@
 function [ result ,wyznaczniki_podmacierzy] = checkSubmatrices( M )
 [m,n]=size(M);
 wyznaczniki_podmacierzy=zeros(m,1);
-result='local max';
+result='local maximum';
 size_M=m;
 M_temp=M;
 %%%%%%%%%%%%%COUNTING DETERMINANTS OF SUBMATRICES%%%%%%%%%%%%%%%%
@@ -14,16 +14,16 @@ end
 [x,y]=size(wyznaczniki_podmacierzy);
 %%%%%%%%%%%%%CHECK WHETHER IS IT A LOCAL MINIMUM%%%%%%%%%%%%%%%%
 if(min(wyznaczniki_podmacierzy)>0)
-    result='local min';
+    result='local minimum';
 else
     %%%%%%%%%%%%%CHECK WHETHER IS IT A LOCAL MAXIMUM%%%%%%%%%%%%%%%%
     for i=x:-1:1
         numberOfSubmatrix = (x - i)+1;
         if(mod(numberOfSubmatrix,2) == 0 && wyznaczniki_podmacierzy(i) < 0) %if i is even
-            result='other';
+            result='local minimum or maximum not achieved';
             break;
         else if(mod(numberOfSubmatrix,2) == 1 && wyznaczniki_podmacierzy(i) > 0)
-                result='other';
+                result='local minimum or maximum not achieved';
                 break;
             end
         end
